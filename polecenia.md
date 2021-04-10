9e9f078283bb0981b92e33d4f49d44cf521e764d
Mateusz Kulesza <mateusz@eduweb.pl>
2021-04-10 12:39:13 +0200

    React VirtualDOM, ReactDOM render, className, style, key

---
+++ polecenia.md
@@ -0,0 +1,76 @@
+
+# Visual Studio Code 
+https://code.visualstudio.com/
+### Help -> About
+code -v 
+1.55.0
+c185983a683d14c396952dd432459097bc7f757f
+x64
+
+# NodeJS
+https://nodejs.org/en/
+node -v
+v14.15.4
+
+npm -v
+6.14.6
+
+# GIT
+https://git-scm.com/download/win
+git --version
+git version 2.28.0.windows.1
+
+## Terminal
+
+## Create React App
+npx create-react-app 
+
+npm i -g create-react-app 
+
+create-react-app --help
+npx create-react-app --help
+
+cd ..
+# Visual Studio Code 
+https://code.visualstudio.com/
+### Help -> About
+code -v 
+1.55.0
+c185983a683d14c396952dd432459097bc7f757f
+x64
+
+# NodeJS
+https://nodejs.org/en/
+node -v
+v14.15.4
+
+npm -v
+6.14.6
+
+# GIT
+https://git-scm.com/download/win
+git --version
+git version 2.28.0.windows.1
+
+## Terminal
+
+## Create React App
+npx create-react-app 
+
+npm i -g create-react-app 
+
+create-react-app --help
+npx create-react-app --help
+
+cd ..
+npx create-react-app lufthansa --template typescript 
+
+cd lufthansa
+npm start 
+
+
+## NPM
+npm install
+npm install -ci 
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.React = React;
+window.ReactDOM = ReactDOM;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const user = { imie: "Bob", pet: 'żyrafę', color: 'blue' }
+const comments = [
+  { id: "123", text: 'Love żyrafę' },
+  { id: "234", text: 'Super Bob!' },
+  { id: "345", text: 'I dont like blue' },
+]
+
+// let wynik = comments.map((item, index) => {
+//   return React.createElement('div', { className: 'list-group-item' }, item.text)
+// })
+
+const vdiv2 = React.createElement('div', {
+  id: 'user-card',
+  className: 'm-4 card',
+  style: { color: '#000', border: '1px solid', borderColor: user.color }
+},
+  React.createElement('div', { className: 'card-body' },
+    React.createElement('h3', null, user.imie),
+    React.createElement('p', null, user.imie + ' ma  ' + user.pet + ' '),
+    React.createElement('div', { className: "list-group list-group-flush" },
+      comments.map((item, index) => {
+        return React.createElement('div', { className: 'list-group-item', key: item.id }, item.text)
+      })
+      // React.createElement('div', { className: 'list-group-item' }, 'Test'),
+      // React.createElement('div', { className: 'list-group-item' }, 'Test'),
+      // React.createElement('div', { className: 'list-group-item' }, 'Test')
+    ),
+    React.createElement('input', { placeholder: 'Dodaj komentarz' }))
+)
+ReactDOM.render(vdiv2, document.getElementById('root'))
+
+
+export { }
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
