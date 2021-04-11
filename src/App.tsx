28c1e0a8f7bf92cdc4da6383d45aeeeab5038bdb
Mateusz Kulesza <mateusz@eduweb.pl>
2021-04-11 10:37:49 +0200

    Bootstrap CSS install

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import './App.css';
 import { PlaylistsView } from './playlists/containers/PlaylistsView';
+
+// npm i bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
