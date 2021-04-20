0af3f25477bf783d81fd08fd4e7d916eb24e9c56
Mateusz Kulesza <mateusz@placki.pl>
2021-04-20 19:46:29 +0200

    Typeahead / debounce - useEffect creator + destrucor - cancelling effect!

--- src/music-search/components/SearchForm.tsx
+++ src/music-search/components/SearchForm.tsx
@@ -7,31 +7,27 @@ interface Props {
 export const SearchForm = ({ onSearch }: Props) => {
     const [query, setQuery] = useState('')
     const queryRef = useRef<HTMLInputElement>(null)
-    // const queryRef2 = useRef<HTMLInputElement>(null)
-
-
-    useEffect(() => { console.log('after each render real dom') })
-    useLayoutEffect(() => { console.log('before render real dom') })
 
     useEffect(() => {
-        console.log('after first render real dom')
-        // document.getElementById('query_id')?.focus()
+        const handle = setTimeout(() => { onSearch(query) }, 500);
+
+        return () => clearTimeout(handle)
+    }, [query])
+
+    useEffect(() => {
         queryRef.current?.focus()
     }, [])
 
-
-    console.log('render vdom')
     return (
         <div>
             <div className="input-group mb-3">
-                {/* <input type="text" onChange={e => setQuery(e.target.value)} ref={queryRef2} /> */}
-                
+
                 <input type="text" className="form-control" placeholder="Search" id="query_id" ref={queryRef}
                     onChange={e => setQuery(e.target.value)}
                     onKeyUp={e => e.code === 'Enter' && onSearch(query)}
                 />
 
-                <button className="btn btn-outline-secondary" type="button" onClick={() => onSearch(query)}>Search</button>
+                {/* <button className="btn btn-outline-secondary" type="button" onClick={() => onSearch(query)}>Search</button> */}
             </div>
         </div>
     )
