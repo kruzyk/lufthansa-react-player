6f28ff5f2334a12d1c6b7df5c0f2b258782535bb
Mateusz Kulesza <mateusz@altkomakademia.pl>
2021-04-25 14:20:47 +0200

    PlaylistsTDD - zadanie opcjonalnie

---
+++ src/playlists/containers/PlaylistsTDD.test.tsx
@@ -0,0 +1,17 @@
+import { PlaylistsTDD } from "./PlaylistsTDD";
+
+describe('PlaylistsTDD', () => {
+
+    test.todo('shows list of no playlists')
+
+    test.todo('shows list of mock playlists')
+    
+    test.todo('selecting playlistlist from list shows details')
+
+    test.todo('clicking edit in details shitches to form')
+
+    /* 
+
+    
+    */
+})
---
+++ src/playlists/containers/PlaylistsTDD.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const PlaylistsTDD = (props: Props) => {
+    return (
+        <div>
+            PlaylistsTDD
+        </div>
+    )
+}
