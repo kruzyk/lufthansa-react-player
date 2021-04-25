af33ff17436943b0214f63c6bbc2241ce44991a9
Mateusz Kulesza <mateusz@altkomakademia.pl>
2021-04-25 13:37:01 +0200

    Async tests, fakeTimers, advanceTime - tesging debounce search form

--- src/core/components/SearchForm.test.tsx
+++ src/core/components/SearchForm.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen, logRoles } from "@testing-library/react"
+import { render, screen, logRoles, fireEvent } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { SearchForm } from "./SearchForm"
 
-describe.only('SearchForm', () => {
+describe('SearchForm', () => {
 
     const setup = () => {
         const searchSpy = jest.fn()
@@ -13,11 +13,39 @@ describe.only('SearchForm', () => {
     test('emits query to parent on enter', () => {
         const { searchSpy, container } = setup()
 
-        const input = screen.getByPlaceholderText('Search',{})
+        const input = screen.getByPlaceholderText('Search', {})
         userEvent.type(input, 'ala ma kota{enter}')
 
         expect(searchSpy).toHaveBeenCalledWith('ala ma kota')
         // logRoles(container)
+    })
+
+
+    test.only('emits query to parent after period of no typing', async () => {
+        jest.useFakeTimers()
+        const { searchSpy, container } = setup()
+        jest.runOnlyPendingTimers();
+
+        const input = screen.getByPlaceholderText('Search', {})
+        // fireEvent.change(input, { target: { value: 'test' } })
+        // Start Timer 500ms
+        userEvent.type(input, 'pierwszy ')
+
+        jest.advanceTimersByTime(500 - 1)
+        expect(searchSpy).not.toHaveBeenCalled()
+
+        // Restart timer 500ms
+        userEvent.type(input, 'i ostatni', {})
+        
+        jest.advanceTimersByTime(500 - 1)
+        expect(searchSpy).not.toHaveBeenCalled()
+        
+        // 499 + 1 = 500ms - Search!
+        jest.advanceTimersByTime(1)
+
+        expect(searchSpy).toHaveBeenCalledWith('pierwszy i ostatni')
+        // logRoles(container)
+        // jest.useRealTimers()
     })
 
 
--- src/core/components/SearchForm.tsx
+++ src/core/components/SearchForm.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 interface Props {
-    onSearch(query: string): void
+    onSearch(query: string): void,
 }
 
 export const SearchForm = ({ onSearch }: Props) => {
     const [query, setQuery] = useState('')
     const queryRef = useRef<HTMLInputElement>(null)
+    const isFirst = useRef(true)
 
     useEffect(() => {
-        const handle = setTimeout(() => { onSearch(query) }, 500);
+        if (isFirst.current) { isFirst.current = false; return; }
+        const handle = setTimeout(() => {
+            onSearch(query)
+        }, 500);
 
         return () => clearTimeout(handle)
     }, [query])
@@ -25,8 +29,8 @@ export const SearchForm = ({ onSearch }: Props) => {
                 <input type="text" className="form-control" placeholder="Search" id="query_id" ref={queryRef}
                     onChange={e => setQuery(e.target.value)}
                     onKeyUp={e => {
+                        // console.log(e.currentTarget.value)
                         // https://create-react-app.dev/docs/debugging-tests/
-debugger
                         e.key === 'Enter' && onSearch(query)
                     }}
                 />
