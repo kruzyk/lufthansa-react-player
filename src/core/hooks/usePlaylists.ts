1bb1943eef5fe9e62e9764ba9ddeea08ced9e54f
Mateusz Kulesza <mateusz@altkomakademia.pl>
2021-04-27 20:18:40 +0200

    MockServiceWorker - mocking server responses

--- src/core/hooks/usePlaylists.ts
+++ src/core/hooks/usePlaylists.ts
@@ -6,8 +6,9 @@ import { useFetch } from "./useFetch"
 // export const usePlaylists = (): [{ isLoading: boolean, message: string, results: Playlist[] | null }, Function] => {
 
 export const fetchPlaylists = (): Promise<Playlist[]> => {
-    throw Error('Not implemented yet!!!')
-    // useFetch(() => axios.get())
+    // throw Error('Not implemented yet!!!')
+    // // useFetch(() => axios.get())
 
+    return axios.get<Playlist[]>('https://api.spotify.com/v1/me/playlists').then(resp => resp.data)
 
 }
--- src/playlists/containers/PlaylistsTDD.test.tsx
+++ src/playlists/containers/PlaylistsTDD.test.tsx
@@ -5,17 +5,41 @@ import { fetchPlaylists } from "../../core/hooks/usePlaylists";
 import { Playlist } from "../../model/Playlist";
 import { PlaylistsTDD } from "./PlaylistsTDD";
 
-jest.mock("../../core/hooks/usePlaylists")
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 
-function mocked<T extends (...args: any) => any>(original: T) {
-    return original as unknown as jest.Mock<ReturnType<T>, Parameters<T>>
-}
-// const fubc = () => {}
-// const fubc = () => {faketimers}
-// const fubc1 = (done) => { promises.then(done)}
-// const fubc2 = async () => { await promise}
+
+// jest.mock("../../core/hooks/usePlaylists")
+
 
 describe('PlaylistsTDD', () => {
+
+    // https://mswjs.io/docs/getting-started/mocks/rest-api
+    const server = setupServer(
+        rest.get('https://api.spotify.com/v1/me/playlists', (req, res, ctx) => {
+            const mockPlaylists: Playlist[] = [
+                { id: '123', name: 'TestTitle 1', description: '', public: false },
+                { id: '234', name: 'TestTitle 2', description: '', public: false },
+            ];
+            ctx.delay(500)
+            return res(ctx.json(mockPlaylists))
+        }),
+        // https://mswjs.io/docs/basics/request-matching
+        // GET https://api.spotify.com/v1/playlists/:playlist_id
+        // req.params.playlist_id
+
+        // PUT https://api.spotify.com/v1/playlists/:playlist_id
+        // req.params.playlist_id
+    )
+
+    // Enable API mocking before tests.
+    beforeAll(() => server.listen())
+
+    // Reset any runtime request handlers we may add during the tests.
+    afterEach(() => server.resetHandlers())
+
+    // Disable API mocking after the tests are done.
+    afterAll(() => server.close())
 
     const setup = () => {
         render(<PlaylistsTDD />)
@@ -24,39 +48,25 @@ describe('PlaylistsTDD', () => {
     // RED -> Green -> Refactor -> RED ...
     test('loads and shows a list playlists', async () => {
         // Arrange - Given ... // no playlists
-        const mockPlaylists: Playlist[] = [
-            { id: '123', name: 'TestTitle 1', description: '', public: false },
-            { id: '234', name: 'TestTitle 2', description: '', public: false },
-        ];
-        jest.useFakeTimers();
-
-        ; (fetchPlaylists as jest.Mock).mockImplementation(() => {
-            return new Promise(resolve => setTimeout(() => resolve(mockPlaylists), 500))
-        })
 
         // Act - When ... // loads playlists
         setup()
         const noItems = screen.queryAllByRole('listitem', {})
         expect(noItems).toHaveLength(0)
-        expect(fetchPlaylists).toHaveBeenCalled()
-
-        jest.advanceTimersToNextTimer()
-
+        
         // Assert - Then ... // Shows list of playlists
-        // const items = await waitFor(() => screen.getAllByRole('listitem', {}))
-
         const items = await screen.findAllByRole('listitem', {})
-        expect(items).toHaveLength(mockPlaylists.length)
+
+        expect(items).toHaveLength(2)
         expect(items[0]).toHaveTextContent('TestTitle 1')
         expect(items[1]).toHaveTextContent('TestTitle 2')
-
     })
 
-    test.todo('shows list of no playlists')
+    // test.todo('shows list of no playlists')
 
-    test.todo('selecting playlistlist from list shows details')
+    test.todo('selecting playlist from list loads details')
 
-    test.todo('clicking edit in details shitches to form')
+    test.todo('clicking edit in details show edit form')
 
     // test('Function adds numbers', () => {
     //     const add = (x:number, y:number) => 5;
