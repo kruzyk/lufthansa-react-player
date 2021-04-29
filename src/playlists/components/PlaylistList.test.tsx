09ee1e1ca8320f4db293d32be11d0b3732bd9f80
Mateusz Kulesza <mateusz@altkomakademia.pl>
2021-04-29 19:55:40 +0200

    TDD - Tests first. Red, Green, Refactor

--- src/playlists/components/PlaylistList.test.tsx
+++ src/playlists/components/PlaylistList.test.tsx
@@ -1,42 +1,86 @@
-import { cleanup, render, screen } from '@testing-library/react'
+import { cleanup, getByAltText, getByLabelText, getByRole, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import React from 'react'
+import { Playlist } from '../../model/Playlist'
 import { PlaylistList } from './PlaylistList'
 
 describe.only('PlaylistList', () => {
-    const playlistsMock = [
-        'Playlista A', 'Playlista B', 'Playlista C',
+    const playlistsMock: Playlist[] = [
+        { id: '123', name: 'Playlista A', public: false, description: '' },
+        { id: '234', name: 'Playlista B', public: false, description: '' },
+        { id: '345', name: 'Playlista C', public: false, description: '' }
     ]
 
-    const setup = ({ playlists = playlistsMock }: { playlists?: string[] }) => {
-        render(<PlaylistList playlists={playlists} />)
-        return { playlists }
+    const setup = ({
+        playlists = playlistsMock,
+        selectedId
+    }: { playlists?: Playlist[], selectedId?: Playlist['id'] }) => {
+        const selectSpy = jest.fn()
+        const removeSpy = jest.fn()
+
+        render(<PlaylistList
+            onRemove={removeSpy
+            }
+            onSelected={selectSpy}
+            selectedId={selectedId}
+            playlists={playlists} />)
+        return { playlists, selectSpy, removeSpy }
     }
 
     // RED -> GREEN -> Refactor
     test('shows "No playlists" message when no playlists', () => {
         setup({ playlists: [] })
-        expect(screen.queryByRole('listitem')).not.toBeInTheDocument()
+        expect(screen.queryByRole('tab')).not.toBeInTheDocument()
         screen.getByText('No playlists')
 
         cleanup()
-        const {playlists} = setup({ })
-        expect(screen.queryAllByRole('listitem')).toHaveLength(playlists.length)
+        const { playlists } = setup({})
+        expect(screen.queryAllByRole('tab')).toHaveLength(playlists.length)
         expect(screen.queryByText('No playlists')).not.toBeInTheDocument()
     })
 
     test('shows list of playlists', () => {
         const { playlists } = setup({})
-        const items = screen.getAllByRole('listitem')
+        const items = screen.getAllByRole('tab')
         for (let index in playlists) {
-            expect(items[index]).toHaveTextContent(playlists[index])
+            expect(items[index]).toHaveTextContent(playlists[index].name)
         }
     })
 
 
-    test.todo('highligts selected playlist')
+    test('highligts selected playlist', () => {
+        setup({ selectedId: '' })
+        const noItems = screen.queryAllByRole('tab', { selected: true })
+        expect(noItems).toHaveLength(0)
 
-    test.todo('emits select event when select clicked')
+        cleanup()
+        const { playlists } = setup({ selectedId: '234' })
+        const items = screen.queryAllByRole('tab', { selected: true })
+        expect(items).toHaveLength(1)
+        expect(playlists[1].id).toEqual('234')
+        expect(items[0]).toHaveTextContent(playlists[1].name)
+        expect(items[0]).toHaveClass('active')
+        // screen.debug(noItems)
+    })
 
-    test.todo('emits remove event when remove clicked')
+    test('emits select event when select clicked', () => {
+        const { selectSpy, playlists } = setup({})
+
+        userEvent.click(screen.getByText(playlists[0].name, { exact: false }))
+
+        expect(selectSpy).toHaveBeenCalledWith(playlists[0].id)
+    })
+
+    test('emits remove event when remove clicked', () => {
+        const { selectSpy, removeSpy, playlists } = setup({})
+
+        const item = screen.getByText(playlists[0].name, { exact: false })
+        const removeBtn = getByRole(item, 'button', { name: 'remove' })
+        userEvent.click(removeBtn)
+
+        expect(removeSpy).toHaveBeenCalledWith(playlists[0].id)
+    })
+
     test.todo('emits create event when create new clicked')
 })
--- src/playlists/components/PlaylistList.tsx
+++ src/playlists/components/PlaylistList.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
 import { Playlist } from '../../model/Playlist'
 
+
 interface Props {
-    playlists: string[]
-    // selectedId?: string
-    // onSelected(id: string): void
-    // onRemove(id: Playlist['id']): void
+    playlists: Playlist[]
+    selectedId?: string
+    onSelected(id: string): void
+    onRemove(id: Playlist['id']): void
 }
 
-export const PlaylistList = ({ playlists }: Props) => {
+export const PlaylistList = ({ playlists, selectedId, onSelected , onRemove}: Props) => {
 
     return (
         <div>
             {!playlists.length && <p>No playlists</p>}
 
-            <ul className="list-group-item">
+            <ul className="list-group list-group-flush" role="tablist">
                 {playlists.map((p, index) =>
-                    <li key={p}>
-                        <span>{index + 1}. {p}</span>
-                    </li>
+                    <li role="tab" key={p.id}
+                        aria-selected={p.id === selectedId}
+                        onClick={() => onSelected(p.id)}
+                        className={"list-group-item list-group-item-action " + (p.id === selectedId ? ' active' : '')}>
+                        {index + 1}. {p.name}
+                        <button className="btn btn-light close" aria-label="remove" onClick={()=>onRemove(p.id)}>&times;</button>                    </li>
                 )}
             </ul>
         </div>
