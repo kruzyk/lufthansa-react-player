ea54935248f8d305314dfd656a8dd6ceae0f2dfa
Mateusz Kulesza <mateusz@altkomakademia.pl>
2021-05-04 19:50:02 +0200

    Testing react hooks with renderHook

--- src/core/hooks/useStorage.test.tsx
+++ src/core/hooks/useStorage.test.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import { renderHook } from '@testing-library/react-hooks'
+import { useStorage } from './useStorage'
+import { mocked } from 'ts-jest'
+/* 
+    const [data, setData] = useStorage('key')
+*/
+
 
 describe('useStorage hook', () => {
 
-    it('store item in storage', () => {
+    const setup = () => {
+        const getItemSpy = jest.spyOn(global.Storage.prototype, 'getItem')
+        const setItemSpy = jest.spyOn(global.Storage.prototype, 'setItem')
+        getItemSpy.mockReturnValue('test123')
 
+        const storageKey = 'somekey'
+        const { result } = renderHook(() => useStorage(storageKey))
+        const [item, setItem] = result.current;
+
+        return {
+            storageKey, item, setItem, getItemSpy, setItemSpy
+        }
+    }
+
+    test('gets item from storage by key', () => {
+        const { getItemSpy, storageKey, item, setItem } = setup()
+        expect(item).toEqual('test123')
+        expect(getItemSpy).toHaveBeenCalledWith(storageKey)
     })
 
-    test('gets item from storage', () => {
+    it('store item in storage in key', () => {
+        const { setItemSpy, storageKey, item, setItem } = setup()
 
+        setItem('placki12')
+        expect(setItemSpy).toHaveBeenCalledWith(storageKey, 'placki12')
     })
+
 })
--- src/core/hooks/useStorage.ts
+++ src/core/hooks/useStorage.ts
@@ -1,3 +1,13 @@
+import { useCallback } from "react"
 
 
-export const useStorage = () => { }
+export const useStorage = (storageKey: string) => {
+
+    const data = sessionStorage.getItem(storageKey)
+
+    const setItem = useCallback((data: any) => {
+        sessionStorage.setItem(storageKey, data)
+    }, [storageKey])
+
+    return [data, setItem] as const
+}
